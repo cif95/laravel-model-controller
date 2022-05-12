f107b97503a8238acbba0a6e3563925eb7e20c92
cif95 <vivianacifarelli9@gmail.com>
2022-05-12 15:54:15 +0200

    create movies cards

--- resources/views/guest/movies/index.blade.php
+++ resources/views/guest/movies/index.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Movies</h1>
-	@dump($movies);
+	<section id="movies" class="container-fluid px-5">
+		<div class="row row-cols-2">
+			@foreach ($movies as $movie)
+
+				<div class="col card" style="width: 18rem;">
+					<div class="card-body">
+						<h5 class="card-title">{{ $movie->title }}</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Original title: {{ $movie->original_title }}</h6>
+						<p class="card-text">Nationality: {{ $movie->nationality }}</p>
+						<p class="card-text">Date: {{ $movie->date }}</p>
+						<p class="card-text">Vote: {{ $movie->vote }}</p>
+					</div>
+				</div>
+
+			@endforeach
+		</div>
+	</section>
 @endsection
